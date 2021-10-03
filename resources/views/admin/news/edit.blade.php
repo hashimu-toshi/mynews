6a18deba72bae514624e35fffb639d7701a53128
EC2 Default User <ec2-user@ip-172-31-0-194.ap-northeast-1.compute.internal>
2021-10-03 12:58:22 +0000

    課題16の提出

--- resources/views/admin/news/edit.blade.php
+++ resources/views/admin/news/edit.blade.php
@@ -5,18 +5,20 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>プロフィール編集</title>
+        <title>ニュース編集</title>
     </head>
     <body>
-        <h1>プロフィール編集画面</h1>
-        @extends('layouts.profile')
-        @section('title', 'プロフィールの編集')
+        <h1>Myニュース編集画面</h1>
+        {{-- layouts/admin.blade.phpを読み込む --}}
+        @extends('layouts.admin')
+        @section('title', 'ニュースの編集')
+        
         @section('content')
             <div class="container">
                 <div class="row">
                     <div class="col-md-8 mx-auto">
-                        <h2>プロフィールの編集</h2>
-                        <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-data">
+                        <h2>ニュース編集</h2>
+                        <form action="{{ action('Admin\NewsController@update') }}" method="post" enctype="multipart/form-data">
                             @if (count($errors) > 0)
                              <ul>
                                  @foreach($errors->all() as $e)
@@ -25,32 +27,33 @@
                              </ul>
                              @endif
                              <div class="form-group row">
-                                 <label class="col-md-2" for="name">名前</label>
+                                 <label class="col-md-2" for="title">タイトル</label>
                                  <div class="col-md-10">
-                                     <input type="text" class="form-control" name="name" value="{{ $profile_form->name }}">
+                                     <input type="text" class="form-control" name="title" value="{{ $news_form->title }}">
                                  </div>
                              </div>
                              <div class="form-group row">
-                                 <label class="col-md-2" for="gender">性別</label>
+                                 <label class="col-md-2" for="bo">本文</label>
                                  <div class="col-md-10">
-                                     <input type="text"  class="form-control" name="gender" value="{{ $profile_form->gender }}">
+                                     <textarea class="form-control" name="body" rows="20">{{ $news_form->body }}</textarea>
                                  </div>
                              </div>
                              <div class="form-group row">
-                                 <label class="col-md-2" for="hobby">趣味</label>
+                                 <label class="col-md-2" for="image">画像</label>
                                  <div class="col-md-10">
-                                     <textarea class="form-control" name="hobby" rows="10">{{ $profile_form->hobby }}</textarea>
-                                 </div>
-                             </div>
-                             <div class="form-group row">
-                                 <label class="col-md-2" for="introduction">自己紹介欄</label>
-                                 <div class="col-md-10">
-                                     <textarea class="form-control" name="introduction" rows="10">{{ $profile_form->introduction }}</textarea>
+                                     <input type="file" class="form-control-file" name="image">
+                                     <div class="form-text text-info">
+                                         設定中: {{ $news_form->image_path }}
+                                     </div>
+                                     <div class="form-check">
+                                         <label class="form-check-label">
+                                             <input type="checkbox" class="form-check-input" name="remove" value="true">画像を削除</label>
+                                     </div>
                                  </div>
                              </div>
                              <div class="form-group row">
                                 <div　class="col-md-10">
-                                    <input type="hidden" name="id" value="{{ $profile_form->id }}"> 
+                                    <input type="hidden" name="id" value="{{ $news_form->id }}"> 
                                     {{ csrf_field() }}
                                     <input type="submit" class="btn btn-primary" value="更新">
                                 </div>
